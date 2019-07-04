24ae8b55318305ca4de384db685aeb7e668d513d
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2019-07-04 18:06:44 +0430

    helpers/ModelLoader: transferred to ts

--- src/helpers/ModelLoader.ts
+++ src/helpers/ModelLoader.ts
@@ -1,10 +1,13 @@
-import Thing from "../vendor/Thing";
+import Thing from "../vendor/Thing"
 
 class Loader extends Thing {
-    startTime = null;
-    endTime = null;
-    object = null
-    isLoaded = false
+
+    startTime: number = 0
+    endTime: number = 0;
+
+    object?: THREE.Object3D;
+    isLoaded: boolean = false
+
     start () {
         this.startTime = Date.now()
         this.emit('start', { at: this.startTime })
@@ -14,12 +17,12 @@ class Loader extends Thing {
             message: `Couldn't fetch the file`
         })
     }
-    progress (xhr) {
+    progress (xhr: $IntentionalAny) {
         this.emit('progress', {
             percent: (xhr.loaded / xhr.total * 100)
         })
     }
-    load (object) {
+    load (object: THREE.Object3D) {
         this.endTime = Date.now()
         const loadTime = this.endTime - this.startTime
         this.isLoaded = true
@@ -30,24 +33,24 @@ class Loader extends Thing {
 
 class ModelLoader extends Thing {
 
-    load (url) {
+    load (url: string) {
         const { $ } = this
         const extension = url.substr(url.lastIndexOf('.') + 1).toLowerCase();
 
         let loader = null;
 
         // OBJ Loader
-        if (extension === 'obj') loader = new $.OBJLoader()
+        if (extension === 'obj') loader = new ($ as any).OBJLoader()
         // JSON Loader
         else if (extension === 'json') loader = new $.ObjectLoader()
 
         if (!loader) console.error(`Couldn't find any loader for .${extension} extension`);
 
         const model = new Loader({ url })
-        loader.load(
+        if (loader) loader.load(
             url,
-            (object) => model.load(object),
-            (xhr) => model.progress(xhr),
+            (object: $FixMe) => model.load(object),
+            (xhr: $FixMe) => model.progress(xhr),
             () => model.error()
         );
         return model;
